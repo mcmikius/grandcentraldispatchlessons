cdad040cb73b08f0dd4c474da8db83eeaa2c15f8
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-08 14:37:03 +0200

    DispatchSemaphore

--- Semaphore.playground/Contents.swift
+++ Semaphore.playground/Contents.swift
@@ -3,5 +3,33 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+let queue = DispatchQueue(label: "mcmikius.semaphore", attributes: .concurrent)
 
+let semaphore = DispatchSemaphore(value: 3)
+semaphore.signal()
 
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 4)
+    print("Block 1")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 2)
+    print("Block 2")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 3")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 4")
+    semaphore.signal()
+}
